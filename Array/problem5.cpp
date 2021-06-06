a14326dcffafd9c21cc05e15ccf2a78e44781f70
Rahul Mishra <rahulmishra102000@gmail.com>
2021-06-06 11:58:43 +0530

    Problem 5 solved

---
+++ Array/problem5.cpp
@@ -0,0 +1,35 @@
+// Problem No. 5 => Move all the negative elements to one side of the array 
+
+#include <iostream>
+using namespace std;
+
+// Rearranging fucntion !!
+void rearrange(int arr[], int n){
+    int j=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]<0){
+            if(i != j){
+                swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+    }
+}
+
+// Printing the new rearranged array.
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[] = {-1, 3, -3, 4, 5, 6, -7, 8, 9};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+
+    rearrange(arr, n);
+    printArray(arr, n);
+
+    return 0; 
+}
