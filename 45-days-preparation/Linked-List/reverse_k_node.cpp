a2421ae8bc7bd58bb15f925e9f8a1eb34b78b483
Rahul Mishra <rahulmishra102000@gmail.com>
2021-07-23 15:04:54 +0530

    45 days => Liked List ---> Reverse K nodes in a linked list

---
+++ 45-days-preparation/Linked-List/reverse_k_node.cpp
@@ -0,0 +1,82 @@
+// Reverse k node in a linked List
+
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+    
+    node* n = new node(val);
+
+    if(head == NULL){
+        head =n;
+        return;
+    }
+    node* temp = head;
+
+    // Traversing the linked list to reach the last emelent
+    while(temp->next!= NULL){
+        temp = temp->next;
+    }
+    temp->next=n;
+}
+
+
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+node* reverseK(node* &head, int k){
+    
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextprt;
+
+    int count = 0;
+    while(currptr != NULL && count<k){
+        nextprt = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextprt;
+        count++;
+    }
+
+    if(nextprt!=NULL){
+        head->next = reverseK(nextprt, k);
+    }
+
+    return prevptr;
+}
+
+int main(){
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    display(head);
+
+
+    int k =2;
+    node* newhead = reverseK(head, k);
+    display(newhead);
+
+    return 0;
+}
