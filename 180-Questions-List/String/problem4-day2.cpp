231a0e14bc8f8837e79f81b70ff99c2e28d6d549
Rahul Mishra <rahulmishra102000@gmail.com>
2021-07-29 15:50:57 +0530

    180 Question => String Day-2 Problem 4 solved

---
+++ 180-Questions-List/String/problem4-day2.cpp
@@ -0,0 +1,40 @@
+// Question => Check for Anagram
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool areAnagram(string str1, string str2){
+    int n1 = str1.length();
+    int n2 = str2.length();
+
+    if(n1 != n2){
+        return false;
+    }
+
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+    for(int i=0; i<n1; i++){
+        if(str1[i] != str2[i]){
+            return false;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    string str1 = "car";
+    string str2 = "arc";
+
+    if(areAnagram){
+        cout<<"Strings are anagram";
+    }
+    else{
+        cout<<"Strings are not anagram";
+    }
+
+    return 0;
+
+}
